8c2e34610f5de61e503074eb38e1ff0535ca01a5
dorayakikun <amakunai.dorayaki@gmail.com>
2017-11-23 11:39:49 +0900

    :art: add getScaleWith

--- src/meshCalculator.js
+++ src/meshCalculator.js
@@ -12,6 +12,44 @@ export type Bounds = {
 export const SCALES = [1, 2, 3]
 
 /**
+ * Get the scale corresponding to zoom.
+ * @param zoom zoom
+ * @returns {number} scale
+ */
+export function getScaleWith(zoom: number): number {
+  switch (zoom) {
+    case 19:
+      return 3
+    case 18:
+      return 3
+    case 17:
+      return 3
+    case 16:
+      return 3
+    case 15:
+      return 3
+    case 14:
+      return 3
+    case 13:
+      return 2
+    case 12:
+      return 2
+    case 11:
+      return 2
+    case 10:
+      return 1
+    case 9:
+      return 1
+    case 8:
+      return 1
+    case 7:
+      return 1
+    case 6:
+      return 1
+  }
+}
+
+/**
  * Convert mesh to LatLng.
  *
  * @param mesh mesh
