73156488694c68ad009e6dcb85645fd2049095a7
dorayakikun <amakunai.dorayaki@gmail.com>
2018-01-03 10:49:52 +0900

    :art: refactoring

--- src/firstMeshCalculator.js
+++ src/firstMeshCalculator.js
@@ -2,27 +2,62 @@
 import type { Bounds, LatLng } from './meshCalculator'
 
 export const meshToLatLng = (meshCode: string): LatLng => {
+  if (!meshCode.match(/\d{4}/)) {
+    throw new Error(
+      `Invalid mesh code found.
+Only numbers are acceptable.
+Actual mesh code is ${meshCode}.`
+    )
+  }
+  const y1 = parseInt(meshCode.substr(0, 2))
+  const x1 = parseInt(meshCode.substr(2))
   return {
-    lat: 0,
-    lng: 0
+    lat: y1 / 1.5 + 1 / 3,
+    lng: x1 + 100 + 1 / 2
   }
 }
 
 export const meshToBounds = (meshCode: string): Bounds => {
+  if (!meshCode.match(/\d{4}/)) {
+    throw new Error(
+      `Invalid mesh code found.
+Only numbers are acceptable.
+Actual mesh code is ${meshCode}.`
+    )
+  }
+
+  const y1 = parseInt(meshCode.substr(0, 2))
+  const x1 = parseInt(meshCode.substr(2, 2))
+  const cy = y1 / 1.5
+  const cx = x1 + 100
+
   return {
-    leftTop: { lat: 0, lng: 0 },
-    rightBottom: { lat: 0, lng: 0 }
+    leftTop: { lat: cy + 2 / 3, lng: cx },
+    rightBottom: { lat: cy, lng: cx + 1 }
   }
 }
 
 export const latLngToMesh = (lat: number, lng: number): string => {
-  return ''
+  const y1 = Math.trunc(lat * 1.5).toString()
+  const x1 = Math.trunc(lng - 100).toString()
+  return y1 + x1
 }
 
 export const panMeshByOffset = (
-  mesh: string,
+  meshCode: string,
   offsetX: number,
   offsetY: number
 ): string => {
-  return ''
+  if (!meshCode.match(/\d{4}/)) {
+    throw new Error(
+      `Invalid mesh code found.
+Only numbers are acceptable.
+Actual mesh code is ${meshCode}.`
+    )
+  }
+
+  const y1 = parseInt(meshCode.substr(0, 2))
+  const x1 = parseInt(meshCode.substr(2))
+
+  return `${y1 + offsetY}${x1 + offsetX}`
 }
