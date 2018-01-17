c0ab11693de45422566012edd889ed1a5923400b
dorayakikun <amakunai.dorayaki@gmail.com>
2018-01-17 22:51:59 +0900

    :hammer: Fix re-export declare

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 // @flow
 
 import {
-  SCALES,
-  scaleFrom,
-  toCenterLatLng,
-  toBounds,
-  toMeshCode,
-  offset,
+  SCALES as _SCALES,
+  scaleFrom as _scaleFrom,
+  toCenterLatLng as _toCenterLatLng,
+  toBounds as _toBounds,
+  toMeshCode as _toMeshCode,
+  offset as _offset,
 } from './meshCalculator'
-import type { LatLng, Bounds } from './meshCalculator'
+import type { LatLng as _LatLng, Bounds as _Bounds } from './meshCalculator'
 
-export { SCALES, scaleFrom, toCenterLatLng, toBounds, toMeshCode, offset }
-export type { LatLng, Bounds }
+export const SCALES = _SCALES
+export const scaleFrom = _scaleFrom
+export const toCenterLatLng = _toCenterLatLng
+export const toBounds = _toBounds
+export const toMeshCode = _toMeshCode
+export const offset = _offset
+export type LatLng = _LatLng
+export type Bounds = _Bounds
