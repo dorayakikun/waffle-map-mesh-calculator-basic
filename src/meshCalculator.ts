a9bddc5863f3c13397856590b64bd0c42398c957
Tomohide Takao <amakunai.dorayaki@gmail.com>
2019-02-11 21:32:49 +0900

    chore: Change ESLint settings

--- src/meshCalculator.ts
+++ src/meshCalculator.ts
@@ -2,40 +2,40 @@ import {
   offset as offsetFirstMeshCode,
   toBounds as firstMeshCodeToBounds,
   toCenterLatLng as firstMeshCodeToCenterLatLng,
-  toMeshCode as toFirstMeshCode,
-} from './firstMeshCalculator'
+  toMeshCode as toFirstMeshCode
+} from "./firstMeshCalculator";
 
 import {
   offset as offsetSecondMeshCode,
   toBounds as secondMeshCodeToBounds,
   toCenterLatLng as secondMeshCodeToLatLng,
-  toMeshCode as toSecondMeshCode,
-} from './secondMeshCalculator'
+  toMeshCode as toSecondMeshCode
+} from "./secondMeshCalculator";
 
 import {
   offset as offsetThirdMeshCode,
   toBounds as thirdMeshCodeToBounds,
   toCenterLatLng as thirdMeshCodeToLatLng,
-  toMeshCode as latLngToThirdMesh,
-} from './thirdMeshCalculator'
+  toMeshCode as latLngToThirdMesh
+} from "./thirdMeshCalculator";
 
 export interface LatLng {
-  lat: number
-  lng: number
+  lat: number;
+  lng: number;
 }
 export interface Bounds {
-  leftTop: LatLng
-  rightBottom: LatLng
+  leftTop: LatLng;
+  rightBottom: LatLng;
 }
 export interface Point {
-  value: number
-  maxDigit: number
+  value: number;
+  maxDigit: number;
 }
 
-export const SCALES = [1, 2, 3]
-export const FIRST_MAX_DIGIT = 99
-export const SECOND_MAX_DIGIT = 7
-export const THIRD_MAX_DIGIT = 9
+export const SCALES = [1, 2, 3];
+export const FIRST_MAX_DIGIT = 99;
+export const SECOND_MAX_DIGIT = 7;
+export const THIRD_MAX_DIGIT = 9;
 
 /**
  * Get the scale corresponding to zoom.
@@ -44,12 +44,12 @@ export const THIRD_MAX_DIGIT = 9
  */
 export const scaleFrom = (zoom: number): number => {
   if (zoom <= 19 && zoom >= 14) {
-    return 3
+    return 3;
   } else if (zoom <= 13 && zoom >= 11) {
-    return 2
+    return 2;
   }
-  return 1
-}
+  return 1;
+};
 
 /**
  * Convert mesh to LatLng.
@@ -58,23 +58,23 @@ export const scaleFrom = (zoom: number): number => {
  * @returns {LatLng} latitude and longitude
  */
 export const toCenterLatLng = (meshCode: string): LatLng => {
-  const newMeshCode = meshCode.replace(/-/g, '')
-  const len = newMeshCode.length
+  const newMeshCode = meshCode.replace(/-/g, "");
+  const len = newMeshCode.length;
   switch (len) {
     case 4:
-      return firstMeshCodeToCenterLatLng(newMeshCode)
+      return firstMeshCodeToCenterLatLng(newMeshCode);
     case 6:
-      return secondMeshCodeToLatLng(newMeshCode)
+      return secondMeshCodeToLatLng(newMeshCode);
     case 8:
-      return thirdMeshCodeToLatLng(newMeshCode)
+      return thirdMeshCodeToLatLng(newMeshCode);
     default:
       throw new Error(
         `Invalid mesh code found.
 The length of the mesh code is 4, 6, or 8.
 The actual length is ${newMeshCode.length}, the mesh code is "${newMeshCode}".`
-      )
+      );
   }
-}
+};
 
 /**
  * Convert mesh to bounds.
@@ -82,24 +82,24 @@ The actual length is ${newMeshCode.length}, the mesh code is "${newMeshCode}".`
  * @returns {Bounds} bounds
  */
 export const toBounds = (meshCode: string): Bounds => {
-  const newMeshCode = meshCode.replace(/-/g, '')
+  const newMeshCode = meshCode.replace(/-/g, "");
 
-  const len = newMeshCode.length
+  const len = newMeshCode.length;
   switch (len) {
     case 4:
-      return firstMeshCodeToBounds(newMeshCode)
+      return firstMeshCodeToBounds(newMeshCode);
     case 6:
-      return secondMeshCodeToBounds(newMeshCode)
+      return secondMeshCodeToBounds(newMeshCode);
     case 8:
-      return thirdMeshCodeToBounds(newMeshCode)
+      return thirdMeshCodeToBounds(newMeshCode);
     default:
       throw new Error(
         `Invalid mesh code found.
 The length of the mesh code is 4, 6, or 8.
 The actual length is ${newMeshCode.length}, the mesh code is "${newMeshCode}".`
-      )
+      );
   }
-}
+};
 
 /**
  * Convert LatLng to mesh.
@@ -112,69 +112,69 @@ The actual length is ${newMeshCode.length}, the mesh code is "${newMeshCode}".`
 export const toMeshCode = (lat: number, lng: number, scale: number): string => {
   switch (scale) {
     case 1:
-      return toFirstMeshCode(lat, lng)
+      return toFirstMeshCode(lat, lng);
     case 2:
-      return toSecondMeshCode(lat, lng)
+      return toSecondMeshCode(lat, lng);
     case 3:
-      return latLngToThirdMesh(lat, lng)
+      return latLngToThirdMesh(lat, lng);
     default:
       throw new Error(
         `Illegal scale found.
 The scale range is [1-3].
 The actual scale is ${scale}.`
-      )
+      );
   }
-}
+};
 
 export const calcNextPoints = (points: Point[]): Point[] => {
-  const nextPoints = [...points]
-  const last = nextPoints.length - 1
-  nextPoints[last].value++
+  const nextPoints = [...points];
+  const last = nextPoints.length - 1;
+  nextPoints[last].value++;
   for (let i = last; i > 0; i--) {
     if (nextPoints[i].value > nextPoints[i].maxDigit) {
-      nextPoints[i].value = 0
-      nextPoints[i - 1].value++
+      nextPoints[i].value = 0;
+      nextPoints[i - 1].value++;
     } else {
-      break
+      break;
     }
   }
-  return nextPoints
-}
+  return nextPoints;
+};
 
 export const calcPrevPoints = (points: Point[]): Point[] => {
-  const prevPoints = [...points]
-  const last = prevPoints.length - 1
-  prevPoints[last].value--
+  const prevPoints = [...points];
+  const last = prevPoints.length - 1;
+  prevPoints[last].value--;
   for (let i = last; i > 0; i--) {
     if (prevPoints[i].value < 0) {
-      prevPoints[i].value = prevPoints[i].maxDigit
-      prevPoints[i - 1].value--
+      prevPoints[i].value = prevPoints[i].maxDigit;
+      prevPoints[i - 1].value--;
     } else {
-      break
+      break;
     }
   }
-  return prevPoints
-}
+  return prevPoints;
+};
 
 export const offset = (
   mesh: string,
   offsetX: number,
   offsetY: number
 ): string => {
-  const newMesh = mesh.replace(/-/g, '')
-  const len = newMesh.length
+  const newMesh = mesh.replace(/-/g, "");
+  const len = newMesh.length;
   switch (len) {
     case 4:
-      return offsetFirstMeshCode(newMesh, offsetX, offsetY)
+      return offsetFirstMeshCode(newMesh, offsetX, offsetY);
     case 6:
-      return offsetSecondMeshCode(newMesh, offsetX, offsetY)
+      return offsetSecondMeshCode(newMesh, offsetX, offsetY);
     case 8:
-      return offsetThirdMeshCode(newMesh, offsetX, offsetY)
+      return offsetThirdMeshCode(newMesh, offsetX, offsetY);
     default:
       throw new Error(
         `Invalid mesh code found.
 The length of the mesh code is 4, 6, or 8.
 The actual length is ${newMesh.length}, the mesh code is "${newMesh}".`
-      )
+      );
   }
-}
+};
--- src/secondMeshCalculator.ts
+++ src/secondMeshCalculator.ts
@@ -1,12 +1,12 @@
-import { toMeshCode as latLngToFirstMesh } from './firstMeshCalculator'
+import { toMeshCode as latLngToFirstMesh } from "./firstMeshCalculator";
 import {
   Bounds,
   calcNextPoints,
   calcPrevPoints,
   FIRST_MAX_DIGIT,
   LatLng,
-  SECOND_MAX_DIGIT,
-} from './meshCalculator'
+  SECOND_MAX_DIGIT
+} from "./meshCalculator";
 
 export const toCenterLatLng = (meshCode: string): LatLng => {
   if (!meshCode.match(/\d{6}/)) {
@@ -14,26 +14,26 @@ export const toCenterLatLng = (meshCode: string): LatLng => {
       `Invalid mesh code found.
 Only numbers are acceptable.
 Actual mesh code is "${meshCode}".`
-    )
+    );
   }
-  const y1 = parseInt(meshCode.substr(0, 2), 10)
-  const x1 = parseInt(meshCode.substr(2, 2), 10)
-  const y2 = parseInt(meshCode.substr(4, 1), 10)
-  const x2 = parseInt(meshCode.substr(5), 10)
+  const y1 = parseInt(meshCode.substr(0, 2), 10);
+  const x1 = parseInt(meshCode.substr(2, 2), 10);
+  const y2 = parseInt(meshCode.substr(4, 1), 10);
+  const x2 = parseInt(meshCode.substr(5), 10);
 
   if (y2 > 7 || x2 > 7) {
     throw new Error(
       `Invalid mesh code found.
 Only [0-7] are acceptable in second division.
 Actual mesh code is "${meshCode}".`
-    )
+    );
   }
 
   return {
     lat: (y1 + y2 / 8) / 1.5 + 1 / 24,
-    lng: x1 + x2 / 8 + 100 + 1 / 16,
-  }
-}
+    lng: x1 + x2 / 8 + 100 + 1 / 16
+  };
+};
 
 export const toBounds = (meshCode: string): Bounds => {
   if (!meshCode.match(/\d{6}/)) {
@@ -41,39 +41,39 @@ export const toBounds = (meshCode: string): Bounds => {
       `Invalid mesh code found.
 Only numbers are acceptable.
 Actual mesh code is "${meshCode}".`
-    )
+    );
   }
 
-  const y1 = parseInt(meshCode.substr(0, 2), 10)
-  const x1 = parseInt(meshCode.substr(2, 2), 10)
-  const y2 = parseInt(meshCode.substr(4, 1), 10)
-  const x2 = parseInt(meshCode.substr(5), 10)
+  const y1 = parseInt(meshCode.substr(0, 2), 10);
+  const x1 = parseInt(meshCode.substr(2, 2), 10);
+  const y2 = parseInt(meshCode.substr(4, 1), 10);
+  const x2 = parseInt(meshCode.substr(5), 10);
 
   if (y2 > 7 || x2 > 7) {
     throw new Error(
       `Invalid mesh code found.
 Only [0-7] are acceptable in second division.
 Actual mesh code is "${meshCode}".`
-    )
+    );
   }
 
-  const cy = (y1 + y2 / 8) / 1.5
-  const cx = x1 + x2 / 8 + 100
+  const cy = (y1 + y2 / 8) / 1.5;
+  const cx = x1 + x2 / 8 + 100;
 
   return {
     leftTop: { lat: cy + 1 / 12, lng: cx },
-    rightBottom: { lat: cy, lng: cx + 1 / 8 },
-  }
-}
+    rightBottom: { lat: cy, lng: cx + 1 / 8 }
+  };
+};
 
 export const toMeshCode = (lat: number, lng: number): string => {
-  const y1 = lat * 1.5
-  const x1 = lng - 100
+  const y1 = lat * 1.5;
+  const x1 = lng - 100;
 
-  const y2 = `${Math.trunc((y1 - Math.trunc(y1)) * 8)}`
-  const x2 = `${Math.trunc((x1 - Math.trunc(x1)) * 8)}`
-  return `${latLngToFirstMesh(lat, lng)}-${y2}${x2}`
-}
+  const y2 = `${Math.trunc((y1 - Math.trunc(y1)) * 8)}`;
+  const x2 = `${Math.trunc((x1 - Math.trunc(x1)) * 8)}`;
+  return `${latLngToFirstMesh(lat, lng)}-${y2}${x2}`;
+};
 
 export const offset = (
   meshCode: string,
@@ -85,35 +85,35 @@ export const offset = (
       `Invalid mesh code found.
 Only numbers are acceptable.
 Actual mesh code is "${meshCode}".`
-    )
+    );
   }
 
-  const y1 = parseInt(meshCode.substr(0, 2), 10)
-  const x1 = parseInt(meshCode.substr(2, 2), 10)
-  const y2 = parseInt(meshCode.substr(4, 1), 10)
-  const x2 = parseInt(meshCode.substr(5), 10)
+  const y1 = parseInt(meshCode.substr(0, 2), 10);
+  const x1 = parseInt(meshCode.substr(2, 2), 10);
+  const y2 = parseInt(meshCode.substr(4, 1), 10);
+  const x2 = parseInt(meshCode.substr(5), 10);
 
-  const calcOffsetY = offsetY > 0 ? calcNextPoints : calcPrevPoints
+  const calcOffsetY = offsetY > 0 ? calcNextPoints : calcPrevPoints;
   let ys = [
     { value: y1, maxDigit: FIRST_MAX_DIGIT },
-    { value: y2, maxDigit: SECOND_MAX_DIGIT },
-  ]
+    { value: y2, maxDigit: SECOND_MAX_DIGIT }
+  ];
   Array(Math.abs(offsetY))
     .fill(0)
     .forEach(() => {
-      ys = calcOffsetY(ys)
-    })
+      ys = calcOffsetY(ys);
+    });
 
-  const calcOffsetX = offsetX > 0 ? calcNextPoints : calcPrevPoints
+  const calcOffsetX = offsetX > 0 ? calcNextPoints : calcPrevPoints;
   let xs = [
     { value: x1, maxDigit: FIRST_MAX_DIGIT },
-    { value: x2, maxDigit: SECOND_MAX_DIGIT },
-  ]
+    { value: x2, maxDigit: SECOND_MAX_DIGIT }
+  ];
   Array(Math.abs(offsetX))
     .fill(0)
     .forEach(() => {
-      xs = calcOffsetX(xs)
-    })
+      xs = calcOffsetX(xs);
+    });
 
-  return `${ys[0].value}${xs[0].value}${ys[1].value}${xs[1].value}`
-}
+  return `${ys[0].value}${xs[0].value}${ys[1].value}${xs[1].value}`;
+};
